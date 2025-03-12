02ab294252c9827d08877dec46e7c0e67a754cb2
kumsa Mergia <kumsamergia@gmail.com>
2025-03-12 13:09:21 +0300

    corrected

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import Sidebar from '../Component/Sidebar/Sidebar'
 import Center from './Center'
-import Album from '../Component/Album/Album'
 import Play from "../Play/Play"
 const Home = () => {
   return (
     <>
     <div className='flex h-screen'>
       <Sidebar />
-      <div className="w-[2px] h-8 bg-gray-300 mx-2"></div>
       <Center />
-      <Album />
-
     </div>
+
     <hr />
+    
     <div>
       <Play /> 
     </div>
