753faa0d0fae66a46e74877131fdb1cba0c527d5
kumsa Mergia <kumsamergia@gmail.com>
2025-03-12 13:09:04 +0300

    corrected

--- src/Pages/Center.jsx
+++ src/Pages/Center.jsx
@@ -5,7 +5,7 @@ import NewReleases from "../Component/NewReleases/NewReleases";
 
 const Center = () => {
   return (
-    <div className="w-240 p-4 overflow-y-auto h-full ml-auto mr-auto">
+    <div className="w-full p-4 overflow-y-auto h-full ml-auto mr-auto">
       
       <PopularAlbum />
       <PopularTracks />
