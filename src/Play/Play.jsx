70dcc2914ab46d2c62843e61eff03c24287ae8b0
kumsa Mergia <kumsamergia@gmail.com>
2025-03-12 13:20:05 +0300

    Play Section

--- src/Play/Play.jsx
+++ src/Play/Play.jsx
@@ -30,7 +30,7 @@ const MediaPlayer = () => {
   };
 
   return (
-    <div className="flex p-4 bg-gray-800 rounded-lg shadow-lg space-x-4 items-center justify-between">
+    <div className="flex p-4 justify-between rounded-lg shadow-lg pr-32">
       <ReactPlayer
         ref={playerRef}
         url="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
@@ -44,13 +44,13 @@ const MediaPlayer = () => {
 
       {/* Controls */}
       <div className="flex items-center gap-7">
-        <button className="text-gray-300 hover:text-white">
+        <button>
           <SkipBack size={24} />
         </button>
-        <button className="text-gray-300 hover:text-white" onClick={togglePlayPause}>
+        <button onClick={togglePlayPause}>
           {playing ? <Pause size={30} /> : <PlayIcon size={30} />}
         </button>
-        <button className="text-gray-300 hover:text-white">
+        <button>
           <SkipForward size={24} />
         </button>
       </div>
@@ -64,24 +64,24 @@ const MediaPlayer = () => {
           max={duration}
           value={progress}
           onChange={handleSeek}
-          className="w-64 h-2 bg-gray-600 rounded-full appearance-none cursor-pointer"
+          className=""
         />
         <span className="text-sm">{Math.floor(duration)}s</span>
       </div>
 
       {/* Other Controls */}
-      <div className="flex items-center space-x-4 mt-2">
-        <button className="text-gray-300 hover:text-white">
+      <div className="flex items-center space-x-2 mt-2">
+        <button>
           <ListMusic size={24} />
         </button>
-        <button className="text-gray-300 hover:text-white">
+        <button>
           <Maximize size={24} />
         </button>
-        <button className="text-gray-300 hover:text-white">
+        <button>
           <Repeat size={24} />
         </button>
         <div className="flex items-center">
-          <Volume2 size={24} className="text-gray-300" />
+          <Volume2 size={24} />
           <input
             type="range"
             min="0"
@@ -89,7 +89,7 @@ const MediaPlayer = () => {
             step="0.1"
             value={volume}
             onChange={(e) => setVolume(parseFloat(e.target.value))}
-            className="w-20 h-2 bg-gray-600 rounded-full appearance-none cursor-pointer"
+            className="w-20"
           />
         </div>
       </div>
