8d9076e14f4525707fd01a1b92a43b872f8620d3
kumsa Mergia <kumsamergia@gmail.com>
2025-03-13 16:43:17 +0300

    changed

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+
   
   plugins: [
     tailwindcss(),
