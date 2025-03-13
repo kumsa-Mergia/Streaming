dda839544ed9d7d3463caa3b9c6e605821e20cd6
kumsa Mergia <kumsamergia@gmail.com>
2025-03-13 16:43:01 +0300

    changed

--- src/Pages/Center.jsx
+++ src/Pages/Center.jsx
@@ -3,12 +3,14 @@ import PopularAlbum from "../Component/PopularAlbum/PopularAlbum";
 import PopularTracks from "../Component/PopularTracks/PopularTracks";
 import NewReleases from "../Component/NewReleases/NewReleases";
 
+
+
 const Center = () => {
   return (
     <div className="w-full p-4 overflow-y-auto h-full ml-auto mr-auto">
       
 
-      <PopularAlbum />
+      <PopularAlbum albumId="4aawyAB9vmqN3uQ7FjRGTy" />
       <PopularTracks />
       <NewReleases />
     </div>
